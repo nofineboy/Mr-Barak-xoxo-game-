96b5e83d11a20e83d2b3ba0ba744a044521823da
nofineboy <promise.nwosu-chimaobi@binghamuni.edu.ng>
2025-07-08 11:12:48 +0100

    Add files via upload

---
+++ XandOVariables.java
@@ -0,0 +1,23 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class XandOVariables {
+
+    ArrayList<Integer> playerOne = new ArrayList<>();
+    ArrayList<Integer> playerTwo = new ArrayList<>();
+    JFrame xand0 = new JFrame("X and 0 Game");
+    JButton btn1 = new JButton();
+    JButton btn2 = new JButton();
+    JButton btn3 = new JButton();
+    JButton btn4 = new JButton();
+    JButton btn5 = new JButton();
+    JButton btn6 = new JButton();
+    JButton btn7 = new JButton();
+    JButton btn8 = new JButton();
+    JButton btn9 = new JButton();
+    JPanel myPanel = new JPanel(new GridLayout(3,3));
+    int flag = 0;
+
+}
