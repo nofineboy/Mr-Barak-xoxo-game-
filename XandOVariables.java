1d9e19c2c8ee6a3506eba3de0d7e4a61dff8fa82
juliusogezigideon <150523551+juliusogezigideon@users.noreply.github.com>
2025-07-09 02:15:29 +0800

    Update XandOVariables.java

--- XandOVariables.java
+++ XandOVariables.java
@@ -1,23 +1,10 @@
+ for (int i = 0; i < 9; i++) {
+            buttons[i] = new JButton("");
+            buttons[i].setFont(new Font("SansSerif", Font.PLAIN, 60));
+            buttons[i].addActionListener(this);
+            add(buttons[i]);
+        }
 
-import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
-
-public class XandOVariables {
-
-    ArrayList<Integer> playerOne = new ArrayList<>();
-    ArrayList<Integer> playerTwo = new ArrayList<>();
-    JFrame xand0 = new JFrame("X and 0 Game");
-    JButton btn1 = new JButton();
-    JButton btn2 = new JButton();
-    JButton btn3 = new JButton();
-    JButton btn4 = new JButton();
-    JButton btn5 = new JButton();
-    JButton btn6 = new JButton();
-    JButton btn7 = new JButton();
-    JButton btn8 = new JButton();
-    JButton btn9 = new JButton();
-    JPanel myPanel = new JPanel(new GridLayout(3,3));
-    int flag = 0;
-
-}
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
