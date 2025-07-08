7cf2a945da1e3158d38306203ff0a3d3e92beafa
juliusogezigideon <150523551+juliusogezigideon@users.noreply.github.com>
2025-07-08 10:02:10 +0100

    Add files via upload

---
+++ XandO.java/src/Main.java
@@ -0,0 +1,12 @@
+void drawGrid(){
+    myPanel.add(btn1);
+    myPanel.add(btn2);
+    myPanel.add(btn3);
+    myPanel.add(btn4);
+    myPanel.add(btn5);
+    myPanel.add(btn6);
+    myPanel.add(btn7);
+    myPanel.add(btn8);
+    myPanel.add(btn9);
+    // Action listeners...
+}
